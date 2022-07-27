8a9f61781e6f7bd112f9cfff370d40598d8864d6
apnavpn <107626617+apnavpn@users.noreply.github.com>
2022-07-27 11:50:09 +0500

    update.js

--- settings.js
+++ settings.js
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is Yasir Tanveer🔥
+My Creator is Yasir Tanveer😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+923154376814'] //Owner number in aive msg
+global.premium = ['+923154376814'] //Owner Number info
+global.ownernomer = '+923154376814' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'Yasir Tanveer' //Owner Name
+global.botname = ' Dark-Bot' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ Youtube ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://youtube.com/c/yasircomputertips' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'Dark-Bot' //Bot Pacage Name
+global.author = 'Yasir Tanveer' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_Yasirtanveer.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://stationeryinn.pk' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
